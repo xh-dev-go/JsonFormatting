a041d0e5a880b546a7a8dd15e5a869d9cfa79f49
xeth <adamhung12@gmail.com>
2022-03-22 01:00:28 +0800

    first version

---
+++ Main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"github.com/atotto/clipboard"
+	"github.com/xh-dev-go/xhUtils/flagUtils"
+	"github.com/xh-dev-go/xhUtils/flagUtils/flagBool"
+	"os"
+)
+
+func main() {
+	prettyPrintVar := flagBool.
+		NewDefault("p", "pretty print json", true).
+		BindCmd().Share("pretty", "pretty print json").BindCmd()
+	uglyPrintVar := flagBool.
+		New("u", "packed json").
+		BindCmd().Share("ugly", "packed json").BindCmd()
+
+	versionVar := flagUtils.Version().BindCmd()
+
+	flag.Parse()
+
+	if len(os.Args) == 1 {
+		flag.PrintDefaults()
+		os.Exit(0)
+	}
+
+	if versionVar.Value() {
+		fmt.Printf("Version: %s", "1.0.0")
+	}
+
+	msg, err := clipboard.ReadAll()
+	if err != nil {
+		panic(err)
+	}
+
+	var m map[string]interface{}
+	err = json.Unmarshal([]byte(msg), &m)
+	if err != nil {
+		panic(err)
+	}
+
+	if uglyPrintVar.Value() {
+		uglyString, err := json.Marshal(m)
+		if err != nil {
+			panic(err)
+		}
+		err = clipboard.WriteAll(string(uglyString))
+		if err != nil {
+			return
+		}
+		err = clipboard.WriteAll(string(uglyString))
+		if err != nil {
+			panic(err)
+		}
+		os.Exit(0)
+	}
+	if prettyPrintVar.Value() {
+		indent, err := json.MarshalIndent(m, "", "  ")
+		if err != nil {
+			panic(err)
+		}
+		err = clipboard.WriteAll(string(indent))
+		if err != nil {
+			panic(err)
+		}
+		os.Exit(0)
+	}
+}
